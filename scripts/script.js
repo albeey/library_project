72b71f1beecc28a535afc7afc375df6e11c053b9
Alberto <malberto24@icloud.com>
2021-11-09 10:56:15 -0600

    Place holder book cover generated

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.cover = generateCover();
 }
 
 function addBookToLibrary() {
@@ -12,9 +13,17 @@ function addBookToLibrary() {
 }
 
 function updateLibrary() {
-  myLibrary.forEach(book => displayBook(book));
+  // const booksInDom = document.querySelectorAll(".title");
+
+  // if (booksInDom) console.log("empty")
+
+  myLibrary.forEach(book => {
+    
+    displayBook(book)
+  });
 }
 
+// Create book on DOM
 function displayBook(book) {
   // Unordered list
   const ul = document.querySelector(".book-list");
@@ -27,6 +36,7 @@ function displayBook(book) {
   // Create book-cover div
   const bookCover = document.createElement("div");
   bookCover.classList.add("book-cover");
+  bookCover.style.background = book.cover;
   li.appendChild(bookCover);
 
   // Create book-info div
@@ -53,6 +63,26 @@ function displayBook(book) {
   bookInfo.appendChild(pages);
 }
 
+// Cover
+function generateCover() {
+  // Generates radial-gradient from random rgb values to use as cover placeholder
+  color1 = generateRgbColor().join(", ");
+  color2 = generateRgbColor().join(", ");
+
+  return `radial-gradient(ellipse at top, rgb(${color1}), transparent), radial-gradient(ellipse at bottom, rgb(${color2}), transparent)`
+}
+
+function generateRgbColor() {
+  rgb = []
+
+  for (let i = 0; i < 3; i++) {
+    color = Math.floor(Math.random() * (255 - 0 + 1) + 0) // generates a random num between 0-255
+    rgb.push(color)
+  }
+  
+  return rgb
+}
+
 const book1 = new Book("Harry Potter and the Philosopher's Stone", "J. K. Rowling", 223, true);
 const book2 = new Book("Harry Potter and the Chamber of Secrets", "J. K. Rowling", 251, false);
 const book3 = new Book("Harry Potter and the Prisoner of Azkaban", "J. K. Rowling", 317, false);
@@ -63,5 +93,5 @@ myLibrary.push(book2)
 myLibrary.push(book3)
 myLibrary.push(book4)
 
-myLibrary.forEach((book) => console.log(book.title))
+myLibrary.forEach((book) => console.log(book))
 updateLibrary()
